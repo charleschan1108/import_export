a0b06f14eb1eebe5e88ded14bdb0e2bfcb55cb7a
Chun Yiu Chan <chunyiuchan@Chuns-MacBook-Air.local>
2021-12-02 14:20:53 +0800

    added comment to config.py

--- config.py
+++ config.py
@@ -1,3 +1,19 @@
+"""
+config.py
+Author: 
+    Charles Chan, Hsueh-i Lu, Rui Pan, Yaheng Wang, Yigang Zhou, Jiaqi Song
+
+Description: 
+    For user to specify the weighitng. 
+    Only the options specifed in the dictionary below are avialble.
+
+Import by:
+    main.py
+
+Import:
+    Nil
+"""
+
 weighting = {
     "vaccinated_rateRank": 0.2,
     "casesRank": 0.2,
